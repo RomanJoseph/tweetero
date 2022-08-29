cd385c230dd6fe9d776015e1e9bb6016baea0fdf
Shizuke136 <romanjose3008@gmail.com>
2022-08-28 22:54:07 -0300

    User avatar bug fixed

--- project/src/server.js
+++ project/src/server.js
@@ -5,33 +5,46 @@ const server = express()
 server.use(express.json());
 server.use(cors())
 
+let actualUser;
 const users = []
 const tweets = [
    {
       username: "bobesponja",
       avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-      tweet: "eu amo o hub"
+      tweet: "Esse é o primeiro"
+   },
+   {
+      username: "kirito",
+      avatar: "https://i.pinimg.com/736x/dd/ef/a5/ddefa530ece368dbd799a4f818c3894e.jpg",
+      tweet: "Eu solo geral"
+   },
+   {
+      username: "doAcoEdElric",
+      avatar: "https://poltronanerd.com.br/wp-content/uploads/2016/06/poltrona-full-metal-filme.jpg",
+      tweet: "É a lei da troca equivalente"
    }
 ]
 
 server.post("/sign-up", (req, res) => {
    users.push(req.body)
+   ActualUser = req.body
    res.send("Ok")
 })
 
 server.post("/tweets", (req, res) => {
    const tweet = {
       username: req.body.username,
-      avatar: users[0].avatar,
+      avatar: actualUser.avatar,
       tweet: req.body.tweet
    }
-   tweets.push(tweet)
-   console.log(tweets)
+   tweets.unshift(tweet)
    res.send("Ok")
 })
 
 server.get("/tweets", (req, res) => {
-   res.send(tweets)
+   const tweetsToSend = tweets.filter((item, index) => index < 10)
+
+   res.send(tweetsToSend)
 })
 
 server.listen(5000)
